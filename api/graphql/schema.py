9a1909327ceac2eb9ffc71c7ab99727b032dc5fe
Sina Shafiee <alisinashafieezada@gmail.com>
2024-11-17 05:23:00 +0330

    feat: collection g-mutations

--- api/graphql/schema.py
+++ api/graphql/schema.py
@@ -2,7 +2,7 @@ import graphene
 
 from apps.poem.graphql.schema import PoemQuery, PoemMutation
 from apps.poet.graphql.schema import PoetQuery, PoetMutation
-from apps.collection.graphql.schema import CollectionQuery
+from apps.collection.graphql.schema import CollectionQuery, CollectionMutation
 from apps.verse.graphql.schema import VerseQuery
 
 
@@ -10,7 +10,7 @@ class Query(PoemQuery, PoetQuery, CollectionQuery, VerseQuery, graphene.ObjectTy
     pass
 
 
-class Mutation(PoemMutation, PoetMutation, graphene.ObjectType):
+class Mutation(PoemMutation, PoetMutation, CollectionMutation, graphene.ObjectType):
     pass
 
 
--- apps/collection/graphql/schema.py
+++ apps/collection/graphql/schema.py
@@ -1,4 +1,6 @@
 import graphene
+
+from api.graphql.permissions import permissions_required
 from .types import CollectionType
 from ..models import Collection
 
@@ -14,3 +16,55 @@ class CollectionQuery(graphene.ObjectType):
     @staticmethod
     def resolve_collection(*args, **kwargs):
         return Collection.objects.get(id=kwargs.get("id"))
+
+
+class CreateCollection(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        type = graphene.String(required=True)
+        description = graphene.String()
+
+    collection = graphene.Field(CollectionType)
+
+    @staticmethod
+    @permissions_required(["collection.add_collection"])
+    def mutate(root, info, title, type, description=None):
+        collection = Collection.objects.create(
+            title=title,
+            type=type,
+            description=description if description else "",
+        )
+        return CreateCollection(collection=collection)
+
+
+class UpdateCollection(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        title = graphene.String()
+        description = graphene.String()
+        type = graphene.String()
+
+    collection = graphene.Field(CollectionType)
+
+    @staticmethod
+    @permissions_required(["collection.change_collection"])
+    def mutate(root, info, id, title=None, description=None, type=None):
+        try:
+            collection = Collection.objects.get(id=id)
+        except Collection.DoesNotExist:
+            raise Exception("Collection not found")
+
+        if title:
+            collection.title = title
+        if type:
+            collection.type = type
+        if description:
+            collection.description = description
+
+        collection.save()
+        return UpdateCollection(collection=collection)
+
+
+class CollectionMutation(graphene.ObjectType):
+    create_collection = CreateCollection.Field()
+    update_collection = UpdateCollection.Field()
