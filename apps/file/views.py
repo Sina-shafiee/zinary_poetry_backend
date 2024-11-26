e180933eeda6495f44c967f02fe1d37996fdbd0c
Sina Shafiee <alisinashafieezada@gmail.com>
2024-11-27 00:22:17 +0330

    fix: added uuid in file_name

--- apps/file/views.py
+++ apps/file/views.py
@@ -1,3 +1,4 @@
+import uuid
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,9 +20,19 @@ class FileUploadView(APIView):
         if serializer.is_valid():
             file = serializer.validated_data["file"]
 
+            file_uuid = uuid.uuid4().hex
+            file_name = f"{file_uuid[:8]}_{file.name.replace(" ", "")}"
+
             file_path = default_storage.save(
-                f"uploads/{file.name}", ContentFile(file.read())
+                f"uploads/{file_name}", ContentFile(file.read())
             )
 
-            return rest_success_response({"url": file_path, "key": file.name})
+            uploaded_file = {
+                "key": file_uuid,
+                "url": default_storage.url(file_path),
+                "name": file_name,
+            }
+
+            return rest_success_response(uploaded_file)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
